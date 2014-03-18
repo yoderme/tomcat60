f88ddac5847ac407584e22bd70de777c9b71cfb3
Mark Emlyn David Thomas <markt@apache.org>
2014-03-18 09:42:35 +0000

    Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=56082 Fix thread safety issue in JULI's LogManager implementation
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc6.0.x/trunk@1578799 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/juli/ClassLoaderLogManager.java
+++ java/org/apache/juli/ClassLoaderLogManager.java
@@ -35,6 +35,7 @@ import java.util.Map;
 import java.util.Properties;
 import java.util.StringTokenizer;
 import java.util.WeakHashMap;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.LogManager;
@@ -676,7 +677,7 @@ public class ClassLoaderLogManager extends LogManager {
 
     protected static final class ClassLoaderLogInfo {
         final LogNode rootNode;
-        final Map<String, Logger> loggers = new HashMap<String, Logger>();
+        final Map<String, Logger> loggers = new ConcurrentHashMap<String, Logger>();
         final Map<String, Handler> handlers = new HashMap<String, Handler>();
         final Properties props = new Properties();
 
