1c006398c0d02013d000bb396defb58e5ee59455
Konstantin Kolinko <kkolinko@apache.org>
2016-01-15 13:29:58 +0000

    Fix https://bz.apache.org/bugzilla/show_bug.cgi?id=58836 Followup. One needs to call queryMB.setCharset() to set encoding for the query string during parameter merging.
    Merged r1724797 from tomcat/tc7.0.x/trunk.
    
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc6.0.x/trunk@1724801 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/core/ApplicationHttpRequest.java
+++ java/org/apache/catalina/core/ApplicationHttpRequest.java
@@ -18,6 +18,7 @@ package org.apache.catalina.core;
 
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.HashMap;
@@ -36,6 +37,7 @@ import org.apache.catalina.Session;
 import org.apache.catalina.Manager;
 import org.apache.catalina.util.Enumerator;
 import org.apache.catalina.util.StringManager;
+import org.apache.tomcat.util.buf.B2CConverter;
 import org.apache.tomcat.util.buf.MessageBytes;
 import org.apache.tomcat.util.http.Parameters;
 
@@ -138,6 +140,8 @@ class ApplicationHttpRequest extends HttpServletRequestWrapper {
     /**
      * The request parameters for this request.  This is initialized from the
      * wrapped request, but updates are allowed.
+     * It is a {@code Map<String,String[]>}, as documented in
+     * ServletRequest.getParameterMap().
      */
     protected Map parameters = null;
 
@@ -869,8 +873,20 @@ class ApplicationHttpRequest extends HttpServletRequestWrapper {
         Parameters paramParser = new Parameters();
         MessageBytes queryMB = MessageBytes.newInstance();
         queryMB.setString(queryParamString);
+
+        String encoding = getCharacterEncoding();
+        // No need to process null value, as ISO-8859-1 is the default encoding
+        // in MessageBytes.toBytes().
+        if (encoding != null) {
+            try {
+                queryMB.setCharset(B2CConverter.getCharset(encoding));
+            } catch (UnsupportedEncodingException ignored) {
+                // Fall-back to ISO-8859-1
+            }
+        }
+
         paramParser.setQuery(queryMB);
-        paramParser.setQueryStringEncoding(getCharacterEncoding());
+        paramParser.setQueryStringEncoding(encoding);
         paramParser.handleQueryParameters();
 
         // Copy the original parameters
