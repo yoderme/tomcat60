81a048eea6a8ec287ab752ecc285658c81852184
Mark Thomas <markt@apache.org>
2016-10-04 06:46:40 +0000

    Improve the access checks for linked global resources to handle the case where the current class loader is a child of the web application class loader.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc6.0.x/trunk@1763237 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/naming/factory/ResourceLinkFactory.java
+++ java/org/apache/naming/factory/ResourceLinkFactory.java
@@ -110,9 +110,12 @@ public class ResourceLinkFactory implements ObjectFactory {
 
     private static boolean validateGlobalResourceAccess(String globalName) {
         ClassLoader cl = Thread.currentThread().getContextClassLoader();
-        Map<String,String> registrations = globalResourceRegistrations.get(cl);
-        if (registrations != null && registrations.containsValue(globalName)) {
-            return true;
+        while (cl != null) {
+            Map<String,String> registrations = globalResourceRegistrations.get(cl);
+            if (registrations != null && registrations.containsValue(globalName)) {
+                return true;
+            }
+            cl = cl.getParent();
         }
         return false;
     }
